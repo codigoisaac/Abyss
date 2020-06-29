75587af5e4a0af927d9e34823c2281058c0ad431
Isaac Muniz <isaacdmuniz@yahoo.com>
2020-06-29 20:31:26 -0300

    Adding HSL color inputs and refactoring

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+// Set Canvas
 const canvas = document.querySelector("#draw");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
+// Set Context
 const ctx = canvas.getContext("2d");
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
@@ -12,13 +13,12 @@ ctx.lineCap = "round";
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
-let hue = 0;
-let direction = false;
-let isResetPoint = true;
 
+// Get mouse events on canvas
 canvas.addEventListener("mousemove", draw);
 canvas.addEventListener("mousedown", (e) => {
   isDrawing = true;
+
   // reset point
   if (isResetPoint) {
     [lastX, lastY] = [e.offsetX, e.offsetY];
@@ -27,11 +27,77 @@ canvas.addEventListener("mousedown", (e) => {
 canvas.addEventListener("mouseup", () => (isDrawing = false));
 canvas.addEventListener("mouseout", () => (isDrawing = false));
 
+// CONTROLS
+// Line Width
+let maxLW = 200;
+const lineWInput = document.querySelector("#lineWidth");
+lineWInput.addEventListener("change", () => {
+  ctx.lineWidth = lineWInput.value;
+  if (lineWInput.value > maxLW) {
+    maxLWInput.value = lineWInput.value;
+  }
+});
+
+// Auto Line Width
+let isAutoLW = true;
+const autoLWInput = document.querySelector("#autoLineWidth");
+autoLWInput.addEventListener("change", () => {
+  isAutoLW = autoLWInput.checked;
+  maxLWInput.disabled = isAutoLW ? false : true; // toggle disabled
+});
+
+// Max Line Width
+const maxLWInput = document.querySelector("#maxLineWidth");
+maxLWInput.addEventListener("change", () => {
+  maxLW = maxLWInput.value;
+  if (maxLW < lineWInput.value) {
+    lineWInput.value = maxLWInput.value;
+  }
+});
+
+// Auto color
+let isAutoColor = true;
+const autoColor = document.querySelector("#autoColor");
+autoColor.addEventListener("change", () => (isAutoColor = autoColor.checked));
+
+// Color hsl
+let hue = 0;
+let saturation = 50;
+let lightness = 50;
+
+// Hue
+const hueIn = document.querySelector("#hue");
+hueIn.addEventListener("change", () => (hue = hueIn.value));
+// Saturation
+const satIn = document.querySelector("#saturation");
+satIn.addEventListener("change", () => (saturation = satIn.value));
+// Lightness
+const ligIn = document.querySelector("#lightness");
+ligIn.addEventListener("change", () => (lightness = ligIn.value));
+
+// Reset Point
+let isResetPoint = true;
+const resetPointInput = document.querySelector("#resetPoint");
+resetPointInput.addEventListener(
+  "change",
+  () => (isResetPoint = !isResetPoint)
+);
+
+let direction = false;
+
 function draw(e) {
   if (!isDrawing) return;
 
+  // color
+  ctx.strokeStyle = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+
   // change color
-  ctx.strokeStyle = `hsl(${hue}, 50%, 50%)`;
+  if (isAutoColor) {
+    hue++;
+    if (hue >= 360) {
+      hue = 0;
+    }
+  }
 
   // Draw!
   ctx.beginPath();
@@ -40,54 +106,15 @@ function draw(e) {
   ctx.stroke();
   // mouse brush (remove to see bug)
   [lastX, lastY] = [e.offsetX, e.offsetY];
-  // change color
-  hue++;
-  if (hue >= 360) {
-    hue = 0;
-  }
 
   // change Line Width
   if (isAutoLW) {
     if (ctx.lineWidth >= maxLW || ctx.lineWidth <= 1) {
-      direction = !direction;
+      direction = !direction; // bug when maxLW > LW
+      // now we have to bring ctx.lineWidth back till it
+      // becomes less than maxLW or more than 1 again
     }
     direction ? ctx.lineWidth++ : ctx.lineWidth--;
     lineWInput.value = ctx.lineWidth;
   }
 }
-
-// Add Controls
-
-let isAutoLW = true;
-let maxLW = 200;
-
-// Line Width
-const lineWInput = document.querySelector("input[id=lineWidth]");
-lineWInput.addEventListener("change", changeLineW);
-
-function changeLineW() {
-  ctx.lineWidth = lineWInput.value;
-  if (lineWInput.value > maxLWInput.value) {
-    maxLWInput.value = lineWInput.value;
-  }
-}
-
-// Auto Line Width
-const autoLWInput = document.querySelector("input[id=autoLineWidth]");
-autoLWInput.addEventListener("change", toggleAutoLW);
-
-function toggleAutoLW() {
-  isAutoLW = autoLWInput.checked;
-  maxLWInput.disabled = isAutoLW ? false : true;
-}
-
-// Max Line Width
-const maxLWInput = document.querySelector("input[id=maxLineWidth]");
-maxLWInput.addEventListener("change", () => (maxLW = maxLWInput.value));
-
-// Reset Point
-const resetPointInput = document.querySelector("input[id=resetPoint");
-resetPointInput.addEventListener(
-  "change",
-  () => (isResetPoint = !isResetPoint)
-);
